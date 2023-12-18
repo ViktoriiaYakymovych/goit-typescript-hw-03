da5f6e89cf2d4b52ec11fa442dc8ab82049dba88
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-12-18 10:50:39 +0200

    tasks 1, 2

--- src/2.ts
+++ src/2.ts
@@ -15,16 +15,11 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -33,7 +28,17 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+  }
+
+  increaseSalary(): number {
+    return (this.salary += 10000);
+  }
 }
 
+// const managerAnn = new Manager("Ann", "manager", 1000);
+// console.log(managerAnn.increaseSalary());
+// managerAnn.increaseSalary();
 
 export {};
