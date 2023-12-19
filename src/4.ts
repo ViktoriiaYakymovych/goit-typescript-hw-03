d959d9d87de58ba381d76e8480fa117452f9f3e6
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-12-19 11:23:36 +0200

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-// const key = new Key();
+class Key {
+  private signature: number;
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+  constructor() {
+    this.signature = Math.random();
+  }
 
-// house.openDoor(person.getKey());
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.comeIn(person);
+class Person {
+  private personKey: Key;
 
+  constructor(key: Key) {
+    this.personKey = key;
+  }
 
-// export {};
+  getKey(): Key {
+    return this.personKey;
+  }
+}
+
+abstract class House {
+  private houseKey: Key;
+  protected door: boolean = false;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.houseKey = key;
+  }
+
+  public comeIn(person: Person): void {
+    this.door && this.tenants.push(person);
+  }
+
+  public abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  private myKey: Key;
+
+  constructor(key: Key) {
+    super(key);
+    this.myKey = key;
+  }
+
+  public openDoor(key: Key): boolean {
+    return key.getSignature() === this.myKey.getSignature();
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
