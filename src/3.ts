9d48f3d9d74f66dfdf51a74a2e542bd9e6f46272
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-12-19 09:47:39 +0200

    task 3

--- src/3.ts
+++ src/3.ts
@@ -8,32 +8,43 @@
 */
 
 // реалізація класу Wizard
-// class Wizard implements ICharacter, ISpellCaster {
-//   constructor(public name: string, public level: number) {
-//     if (this.level < 1) {
-//       throw new Error('Level too low');
-//     }
-//   }
 
-//   introduce(phrase: string): void {
-//     console.log(phrase + ', ' + this.name);
-//   }
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
 
-//   castSpell(): void {
-//     console.log('Casting a spell, behold my power!');
-//   }
+interface ISpellCaster {
+  castSpell(): void;
+}
+class Wizard implements ICharacter, ISpellCaster {
+  constructor(public name: string, public level: number) {
+    if (this.level < 1) {
+      throw new Error("Level too low");
+    }
+  }
 
-//   levelUp(): void {
-//     this.level++;
-//     console.log(`Level up! New level is ${this.level}`);
-//   }
-// }
+  introduce(phrase: string): void {
+    console.log(phrase + ", " + this.name);
+  }
 
-// // тестування класу
-// const wizard = new Wizard('Merlin', 15);
+  castSpell(): void {
+    console.log("Casting a spell, behold my power!");
+  }
 
-// wizard.introduce('I am the mighty wizard');
-// wizard.castSpell();
-// wizard.levelUp();  // Level up! New level is 16
+  levelUp(): void {
+    this.level++;
+    console.log(`Level up! New level is ${this.level}`);
+  }
+}
 
-// export {};
+// тестування класу
+const wizard = new Wizard("Merlin", 15);
+
+wizard.introduce("I am the mighty wizard");
+wizard.castSpell();
+wizard.levelUp(); // Level up! New level is 16
+
+export {};
